461e14f10189ab1a5b57867202d111feaa08db6f
Mohammad Rashid <dopplesoldner@gmail.com>
2016-06-17 11:23:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,13 @@
 
 #install vundle
 echo "Cloning Vundle"
-mv -f ~/.vim ~/.vim.bak
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #add soft links
 echo "Creating RC files"
 d=`pwd`
-mv -f ~/.vimrc ~/.vimrc.bak
-#mv -f ~/.screenrc ~/.screenrc.bak
 ln -f -s $d/vimrc ~/.vimrc
-#ln -f -s $d/screenrc ~/.screenrc
+ln -f -s $d/screenrc ~/.screenrc
 
 ##colors
 echo "Setting vim colours"
